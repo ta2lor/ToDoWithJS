b672f86f19d640e91524514901f96c54635182b2
hyoseok <ksh262011@naver.com>
2020-09-25 23:32:49 +0900

    first commit

---
+++ clock.js
@@ -0,0 +1,18 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  clockTitle.innerHTML = `${hours > 9 ? hours : `0${hours}`} : ${
+    minutes < 10 ? `0${minutes}` : minutes
+  } : ${seconds > 9 ? seconds : `0${seconds}`}`;
+}
+function init() {
+  setInterval(getTime, 1000);
+}
+
+init();
